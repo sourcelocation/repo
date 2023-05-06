5dda83d1721850c7bfac26d44b8da04c885f1c6f
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-05-06 21:26:21 +0100

    try fixing rootful

--- repo.sh
+++ repo.sh
@@ -5,7 +5,12 @@ cd $script_full_path || exit 1
 rm Packages Packages.bz2 Packages.xz Packages.zst
 
 echo "[Repository] Appending debs to Packages..."
-dpkg-scanpackages debs /dev/null > Packages
+dpkg-scanpackages debs/iphoneos-arm /dev/null | gzip -9c > Packages-iphoneos-arm
+dpkg-scanpackages debs/iphoneos-arm64 /dev/null | gzip -9c > Packages-Packages-iphoneos-arm64
+
+cat Packages-iphoneos-arm Packages-iphoneos-arm64 > Packages
+
+echo "[Repository] Compressing Packages..."
 
 zstd -q -c19 Packages > Packages.zst
 xz -c9 Packages > Packages.xz
